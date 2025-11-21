41adb51a8ee088f106ad7001e9f3ed347fd0ed26
Teresa-perezpozo <t.perezdearenaza@alumnos.upm.es>
2025-11-21 13:38:35 +0100

    terminada practica 5

--- src/es/upm/dit/aled/lab5/AreaQueue.java
+++ src/es/upm/dit/aled/lab5/AreaQueue.java
@@ -13,13 +13,43 @@ import es.upm.dit.aled.lab5.gui.Position2D;
  * @author rgarciacarmona
  */
 public class AreaQueue extends Area {
+//	public AreaQueue (String name, int capacity, Position2D position) {
+//		this.name = name;
+//		this.capacity = capacity;
+//		this.position=position;
+//	}
+//	2 métodos y 1 atributo
+	//he puesto el horno a y 10
 	
-	public AreaQueue (String name, int capacity, Position2D position) {
-		this.name = name;
-		this.capacity = capacity;
-		this.position=position;
+	public AreaQueue(String name, int time, int capacity, Position2D position) {
+		super(name, time, capacity, position);
+		
 	}
-	
 	private LinkedList<Patient> Queue = new LinkedList<>();
-	
+	@Override
+	public synchronized void enter(Patient p) {
+		System.out.println("Patient " + p.getNumber() + " trying to enter " + this.name);
+
+		try {
+			Queue.add(p);
+			waiting++;//se me había olvidado
+		
+		while(this.numPatients>=this.capacity||this.Queue.peek() != p) {
+			System.out.println(" el paciente" + p.getNumber()+ " se pone a la cola ");
+			
+			wait();
+			
+		}
+
+		this.numPatients++;
+		this.waiting--;
+		System.out.println(" el paciente" + Queue.peek()+ "por fín entra en "+ this.name);
+		Queue.remove(p);
+
+		
+		}catch(InterruptedException e ) {
+			e.printStackTrace();
+			Thread.currentThread().interrupt();
+		}
+}
 }
